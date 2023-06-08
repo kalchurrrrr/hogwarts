297eafa6b04bc691334d47225a704bd21179159d
kalchurrrrr <nikolaykalchu@gmail.com>
2023-06-08 18:58:09 +0300

    Сделал дз, проверьте пожалуйста!

---
+++ src/Hufflepuff.java
@@ -0,0 +1,16 @@
+class Hufflepuff {
+    private int loyalty;
+    private int patience;
+    public Hufflepuff(int loyalty, int patience) {
+        this.loyalty = loyalty;
+        this.patience = patience;
+    }
+
+    public int getLoyalty() {
+        return loyalty;
+    }
+
+    public int getPatience() {
+        return patience;
+    }
+}
